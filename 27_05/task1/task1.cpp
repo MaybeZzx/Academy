24f4790a27aaea31044588e488e6694d566eb37c
P311 <P311@ST-5-11>
2024-05-29 20:20:58 +0300

    complete task

--- 27_05/task1/task1.cpp
+++ 27_05/task1/task1.cpp
@@ -1,21 +1,91 @@
-#include <iostream>
-#include <string>
-#include <cstring>
+#include "../../lib.h"
+#include <ctype.h>
+
 using namespace std;
 
-string ReplaceWord(string str, string lastWord, string newWord)
+void ReplaceWord(string &str, string lastWord, string newWord)
 {
     while (str.find(lastWord) != -1)
     {
         str.insert(str.find(lastWord), newWord); // Вставляем новое слово перед старым
         str.erase(str.find(lastWord), lastWord.length()); // Находим и удаляем старое слово
     }
-
-    return str;
 }
+void TextToUpper(string &str)
+{
+    bool mark = false;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (isspace(str[i]))
+            continue;
 
+        if (str[i] == '.' || str[i] == '!' || str[i] == '?')
+            mark = true;
+        else if (mark)
+        {
+            str[i] = toupper(str[i]);
+            mark = false;
+        }
+    }
+}
+void CountLetters(string& str)
+{
+    map<char, int> letters;
+    transform(str.begin(), str.end(), str.begin(), tolower);
+    cout << str << endl;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (isalpha(str[i]) && letters.contains(str[i]))
+        {
+            letters[str[i]]++;
+        }
+        else if (isalpha(str[i]) && !letters.contains(str[i]))
+        {
+            letters[str[i]] = 1;
+        }
+        else
+            continue;
+
+    }
+    for (auto [letter, num] : letters)
+    {
+        cout << letter << ": " << num << endl;
+    }
+}
+void CountDigits(string& str)
+{
+    map<char, int> digits;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (isdigit(str[i]) && digits.contains(str[i]))
+        {
+            digits[str[i]]++;
+        }
+        else if (isdigit(str[i]) && !digits.contains(str[i]))
+        {
+            digits[str[i]] = 1;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    if (digits.empty())
+    {
+        cout << "В строке нет цифр" << endl;
+        return;
+    }
+    for (auto [digit, num] : digits)
+    {
+        cout << digit << ": " << num << endl;
+    }
+}
 int main()
 {
-    string str1 = "hello world hello hello";
-    cout << ReplaceWord(str1, "hello", "world");
+    setlocale(LC_ALL, "RUS");
+    string str1 = "Hello hello. world.";
+    /*ReplaceWord(str1, "hello", "world");
+    cout << str1 << endl;
+    TextToUpper(str1);*/
+    CountDigits(str1);
 }
---
+++ lib.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::map;
+
