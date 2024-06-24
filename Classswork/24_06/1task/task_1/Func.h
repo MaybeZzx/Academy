7c9952cf89f8978e22c90c7f41087958680231c5
P311 <P311@D23>
2024-06-24 20:14:34 +0300

    add 24_06 1task practice

---
+++ Classswork/24_06/1task/task_1/Func.h
@@ -0,0 +1,7 @@
+#include <string>
+
+int FindWord(std::string text, std::string wordToFind);
+int CountPoint(const std::string& text);
+int* CountPunct(std::string& const text);
+void SwapText(std::string& text);
+void SwapSentences(std::string& text);
