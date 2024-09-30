8d1c643f167ba003351ee8ca3f8e19520600bdc8
maybe_zzz <166614674+MaybeZzx@users.noreply.github.com>
2024-09-30 16:51:07 +0300

    /

--- Homework/week_15/task_1/Func.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-double Action(int* a, int* b, int const& size_A, int const& size_B, double(*func)(int*, int*, int const&, int const&));
-double max(int* a, int* b, int const& size_A, int const& size_B);
-double min(int* a, int* b, int const& size_A, int const& size_B);
-double avg(int* a, int* b, int const& size_A, int const& size_B);
